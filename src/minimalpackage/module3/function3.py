7e12460b24032b30affebaa703134f58930f7ea0
gilbertohasnofb <gilbertohasnofb@gmail.com>
2026-07-03 21:16:36 +0100

    fix ruff warnings

--- src/minimalpackage/module3/function3.py
+++ src/minimalpackage/module3/function3.py
@@ -6,5 +6,6 @@ DATA_FOLDER_PATH = Path(__file__).parent.parent / "data"
 
 
 def function3() -> pd.DataFrame:
+    """Doctsring"""
     data_path = DATA_FOLDER_PATH / "sample.json"
     return pd.read_json(data_path)
